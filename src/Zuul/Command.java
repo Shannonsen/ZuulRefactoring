3951b8495feb8c49c1d92b44017fafc1e9f649b9
Noe-Alejandro <A18016311@alumnos.uady.mx>
2021-02-02 19:26:07 -0500

    Se eliminaron comentarios obligatorios(params): Command[(26-32)]

--- src/Zuul/Command.java
+++ src/Zuul/Command.java
@@ -23,13 +23,6 @@ public class Command
     private String commandWord;
     private String secondWord;
 
-    /**
-     * Create a command object. First and second word must be supplied, but
-     * either one (or both) can be null.
-     * @param firstWord The first word of the command. Null if the command
-     *                  was not recognised.
-     * @param secondWord The second word of the command.
-     */
     public Command(String firstWord, String secondWord)
     {
         commandWord = firstWord;
